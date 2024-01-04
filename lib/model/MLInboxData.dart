84fc51587b0a020b505cb676b60b4b14c120a019
Yuan <waiiyuanchai21@gmail.com>
2024-01-04 22:14:00 +0800

    connected chatbot to backend

--- lib/model/MLInboxData.dart
+++ lib/model/MLInboxData.dart
@@ -2,19 +2,17 @@ class MLInboxData {
   int? id;
   String? role;
   String? parts;
+  int? type;
 
-  MLInboxData({this.id, this.parts, this.role});
+  MLInboxData({this.id, this.parts, this.role, this.type});
 
   MLInboxData.fromJson(Map json)
       : id = json['id'] as int?,
         role = json['role'] as String?,
-        parts = json['parts'] as String?;
+        parts = json['parts'] as String?,
+        type = json['type'] as int?;
 
   Map toJson() {
-    return {
-      'id': id,
-      'role': role,
-      'parts': parts,
-    };
+    return {'id': id, 'role': role, 'parts': parts, 'type': type};
   }
 }
